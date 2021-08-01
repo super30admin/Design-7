8485edd651ef00a080ac3e8b64056ade4bc00a6d
manojvarmarudraraju <46211625+manojvarmarudraraju@users.noreply.github.com>
2021-08-01 16:26:08 +0530

    Completed Problem 2

--- Problem2.py
+++ Problem2.py
@@ -1,5 +1,5 @@
-# Time Complexity : O()
-# Space Complexity : O()
+# Time Complexity : O(n) for move n is size of food
+# Space Complexity : O(n)
 # Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : No
 
