aaaad65b66d1d4b3343fddeba9da944714050b89
kgajwan1 <36811665+kgajwan1@users.noreply.github.com>
2021-08-04 19:15:36 -0400

    O(n) + O(n) TC + SC

---
+++ Problem-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+  
+  int hIndex(vector<int>& citations) {
+std::sort(citations.begin(), citations.end(), greater<int>());
+    for (int i=0; i<citations.size(); i++)
+       if (i>=citations[i]) return i;
+    return citations.size();
+ /*   if(citations.empty())
+        return 0;
+    int n = citations.size();
+    vector<int> hash(n + 1, 0);
+    for(int i = 0; i < n; ++i){
+        if(citations[i] >= n)
+            hash[n]++;
+        else
+            hash[citations[i]]++;
+    }
+    int paper = 0;
+    for(int i = n; i >= 0; --i){
+        paper += hash[i];
+        if(paper >= i)
+            return i;
+    }
+    return paper;*/
+}
+};
