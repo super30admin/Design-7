ef9fa8587489b118ae3781f2c908b98787e36631
pragyas03 <79885052+pragyas03@users.noreply.github.com>
2021-08-28 10:06:55 -0700

    Update SnakeGame.java

--- SnakeGame.java
+++ SnakeGame.java
@@ -1,3 +1,7 @@
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+
 class SnakeGame {
     /** Initialize your data structure here.
         @param width - screen width
