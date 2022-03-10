193005a4444abbf5f801726fd6b09bec73210971
LiberoPat <patrick.trias@gmail.com>
2022-03-09 16:16:38 -0800

    complete

---
+++ lfuCache.py
@@ -0,0 +1,144 @@
+import collections
+"""
+time: O(1)
+space: O(N)
+"""
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.freq = 1
+        self.prev = self.next = None
+
+class DLinkedList:
+
+    def __init__(self):
+        self._sentinel = Node(None, None) # dummy node
+        self._sentinel.next = self._sentinel.prev = self._sentinel
+        self._size = 0
+    
+    def __len__(self):
+        return self._size
+    
+    def append(self, node):
+        node.next = self._sentinel.next
+        node.prev = self._sentinel
+        node.next.prev = node
+        self._sentinel.next = node
+        self._size += 1
+    
+    def pop(self, node=None):
+        if self._size == 0:
+            return
+        
+        if not node:
+            node = self._sentinel.prev
+
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        self._size -= 1
+        
+        return node
+        
+class LFUCache:
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        
+        Three things to maintain:
+        
+        1. a dict, named as `self._node`, for the reference of all nodes given key.
+           That is, O(1) time to retrieve node given a key.
+           
+        2. Each frequency has a doubly linked list, store in `self._freq`, where key
+           is the frequency, and value is an object of `DLinkedList`
+        
+        3. The min frequency through all nodes. We can maintain this in O(1) time, taking
+           advantage of the fact that the frequency can only increment by 1. Use the following
+		   two rules:
+           
+           Rule 1: Whenever we see the size of the DLinkedList of current min frequency is 0,
+                   the min frequency must increment by 1.
+           
+           Rule 2: Whenever put in a new (key, value), the min frequency must 1 (the new node)
+           
+        """
+        self._size = 0
+        self._capacity = capacity
+        
+        self._node = dict() # key: Node
+        self._freq = collections.defaultdict(DLinkedList)
+        self._minfreq = 0
+        
+        
+    def _update(self, node):
+        """ 
+        """
+        freq = node.freq
+        
+        self._freq[freq].pop(node)
+        if self._minfreq == freq and not self._freq[freq]:
+            self._minfreq += 1
+        
+        node.freq += 1
+        freq = node.freq
+        self._freq[freq].append(node)
+    
+    def get(self, key):
+        """
+        Through checking self._node[key], we can get the node in O(1) time.
+        Just performs self._update, then we can return the value of node.
+        
+        :type key: int
+        :rtype: int
+        """
+        if key not in self._node:
+            return -1
+        
+        node = self._node[key]
+        self._update(node)
+        return node.val
+
+    def put(self, key, value):
+        """
+        If `key` already exists in self._node, we do the same operations as `get`, except
+        updating the node.val to new value.
+        
+        Otherwise, the following logic will be performed
+        
+        1. if the cache reaches its capacity, pop the least frequently used item. (*)
+        2. add new node to self._node
+        3. add new node to the DLinkedList with frequency 1
+        4. reset self._minfreq to 1
+        
+        (*) How to pop the least frequently used item? Two facts:
+        
+        1. we maintain the self._minfreq, the minimum possible frequency in cache.
+        2. All cache with the same frequency are stored as a DLinkedList, with
+           recently used order (Always append at head)
+          
+        Consequence? ==> The tail of the DLinkedList with self._minfreq is the least
+                         recently used one, pop it...
+        
+        :type key: int
+        :type value: int
+        :rtype: void
+        """
+        if self._capacity == 0:
+            return
+        
+        if key in self._node:
+            node = self._node[key]
+            self._update(node)
+            node.val = value
+        else:
+            if self._size == self._capacity:
+                node = self._freq[self._minfreq].pop()
+                del self._node[node.key]
+                self._size -= 1
+                
+            node = Node(key, value)
+            self._node[key] = node
+            self._freq[1].append(node)
+            self._minfreq = 1
+            self._size += 1
